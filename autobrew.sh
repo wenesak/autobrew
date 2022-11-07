6e2788e53ba9f2fcf0c60a1573d33c3ff79e80c7
wenesak <wene.s@hotmail.com>
2022-11-06 21:41:02 -0600

    Update autobrew.sh

--- autobrew.sh
+++ autobrew.sh
@@ -228,7 +228,7 @@ main() {
     # Leave list blank or comment out the list if not required.
     tap_list="homebrew/cask-fonts zsh zsh-autosuggestions"
     term_list="git wget curl " #tmux
-    cask_list=" visual-studio-code  docker iterm2 " #google-chrome
+    cask_list=" visual-studio-code  docker iterm2 zsh-autosuggestions" #google-chrome
     clear
     term_colors
     script_info
