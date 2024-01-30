bc83e4937466e8ba7d8c49b7d0c22febfe84a9c1
wenesak <wene.s@hotmail.com>
2024-01-30 17:48:20 -0600

    Update autobrew.sh

--- autobrew.sh
+++ autobrew.sh
@@ -228,7 +228,7 @@ main() {
     # Customise the following list variables (tap_list, term_list and cask_list) 
     # Leave list blank or comment out the list if not required.
     tap_list="homebrew/cask-fonts zsh "
-    term_list="git wget curl " #tmux
+    term_list="git wget curl azure-cli" #tmux
     cask_list=" visual-studio-code  docker iterm2" #google-chrome
     clear
     term_colors
